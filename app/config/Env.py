b3a86fe0a16a500f7d71c773f17e8790c532d363
webmis <klingsoul@163.com>
2026-02-04 16:57:32 +0800

    生产环境配置

--- app/config/Env.py
+++ app/config/Env.py
@@ -1,6 +1,6 @@
 # 公共配置
 class Env:
-  mode: str = 'dev'                              # 开发环境: dev
+  mode: str = ''                                 # 开发环境: dev
   title: str = 'WebMIS 3.0'                      # 项目名称
   copy: str = 'webmis.vip &copy; 2026'           # 版权
   server_host: str = 'localhost'                 # 服务器地址
--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 #!/bin/python
 # -*- coding: UTF-8 -*-
 from core.Server import WSGIApplication
+from wsgiref.simple_server import make_server, WSGIRequestHandler
 from app.config.Env import Env
+
+# 屏蔽日志
+class SilentRequestHandler(WSGIRequestHandler):
+  def log_request(self, code='-', size='-'): pass
 
 # 实例化
 app = WSGIApplication()
 if __name__ == '__main__':
-  from wsgiref.simple_server import make_server
   # 开发
   if(Env.mode=='dev'):
-    print('[ Server ]', f"http://{Env.server_host}:{Env.server_port}")
-  # 服务
-  server = make_server(Env.server_host, Env.server_port, app)
+    server = make_server(Env.server_host, Env.server_port, app)
+  else:
+    server = make_server(Env.server_host, Env.server_port, app, handler_class=SilentRequestHandler)
+  # 运行
+  print('[ Server ]', f"http://{Env.server_host}:{Env.server_port}")
   server.serve_forever()
