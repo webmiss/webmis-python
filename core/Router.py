c0211b6450a575f4ad1714fcacfb8272e4616bca
webmis <klingsoul@163.com>
2026-03-21 15:06:40 +0800

    控制器、函数命名规范

--- core/Router.py
+++ core/Router.py
@@ -12,8 +12,8 @@ class Router:
     # URL
     parts = path.strip('/').split('/')
     module = parts[0] if len(parts) > 0 else self.__module
-    controller = parts[1][0].upper() + parts[1][1:] if len(parts) > 1 else self.__controller
-    method = parts[2][0].upper() + parts[2][1:] if len(parts) > 2 else self.__method
+    controller = ''.join(word.capitalize() for word in parts[1].split('_')) if len(parts) > 1 else self.__controller
+    method = ''.join(word.capitalize() for word in parts[2].split('_')) if len(parts) > 2 else self.__method
     params = parts[3:] if len(parts) > 3 else self.__params
     # 返回
     return module, controller, method, params
